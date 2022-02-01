ea57d6e30b0c43d44d7c69f7e6e34f2f604d8f04
김정환 <ej88ej@gmail.com>
2022-02-01 22:54:33 +0900

    yup 추가

--- src/SignupFormFormik.tsx
+++ src/SignupFormFormik.tsx
@@ -1,11 +1,14 @@
 import { useFormik } from "formik";
 import { FC } from "react";
+import * as Yup from "yup";
 
-interface SignupFormValue {
-  firstName: string;
-  lastName: string;
-  email: string;
-}
+const validationSchema = Yup.object({
+  firstName: Yup.string().required("Required").max(15),
+  lastName: Yup.string().required("Required"),
+  email: Yup.string().required("Required"),
+});
+
+type SignupFormValue = Yup.InferType<typeof validationSchema>;
 
 const SignupFormFormik: FC = () => {
   const formik = useFormik<SignupFormValue>({
@@ -14,36 +17,52 @@ const SignupFormFormik: FC = () => {
       lastName: "",
       email: "",
     },
+    validationSchema,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 4));
     },
   });
   return (
     <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="firstName">First Name:</label>
-      <input
-        id="firstName"
-        name="firstName"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.firstName}
-      />
-      <label htmlFor="firstName">Last Name:</label>
-      <input
-        id="lastName"
-        name="lastName"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.lastName}
-      />
-      <label htmlFor="email">Email Address:</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
+      <div>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          id="firstName"
+          name="firstName"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.firstName}
+        />
+        {formik.touched.firstName && formik.errors.firstName && (
+          <p>{formik.errors.firstName}</p>
+        )}
+      </div>
+      <div>
+        <label htmlFor="firstName">Last Name:</label>
+        <input
+          id="lastName"
+          name="lastName"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.lastName}
+        />
+        {formik.touched.lastName && formik.errors.lastName && (
+          <p>{formik.errors.lastName}</p>
+        )}
+      </div>
+      <div>
+        <label htmlFor="email">Email Address:</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+      </div>
       <button type="submit">Submit</button>
     </form>
   );
